3bb61d79bffe294b9b2e4481dd5fdcb3fbe7abce
Christopher Noce <c@abdefg.com>
2019-01-18 14:35:06 -0500

    Update calendar.js

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,30 +1,15 @@
 $(document).ready(function() {
   $('#foobar').fullCalendar({
-    googleCalendarApiKey: 'AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ',
     eventSources: [
-      {
-        googleCalendarId: 'concordps.org_classroom6880af80@group.calendar.google.com',
-        className: '6band'
-      },
-      {
-        googleCalendarId: 'concordps.org_classroom60624685@group.calendar.google.com',
-        className: '78band'
-      },
-      {
-        googleCalendarId: 'concordps.org_nbu2m6begv8r25refgt02umj1c@group.calendar.google.com',
-        className: 'elementary'
-      },
-      {
-        googleCalendarId: 'concordps.org_classroomfa6b71ca@group.calendar.google.com',
-        className: 'jazz'
-      },
-      {
-        googleCalendarId: 'concordps.org_classroom6d3a6565@group.calendar.google.com',
-        className: 'windensemble'
-      }
-    ]
-  });
-});
+      // your event source
+        {
+          url: 'https://ical-to-json.herokuapp.com/convert.json?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fconcordps.org_classroom6880af80%2540group.calendar.google.com%2Fpublic%2Fbasic.ics', // use the `url` property
+          color: 'yellow',    // an option!
+          textColor: 'black'  // an option!
+        }
 
-// AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ
-// AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs
+        // any other sources...
+
+      ]
+    });
+  });
