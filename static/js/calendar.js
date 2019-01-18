5427649fb5d56022367e4d5105d8486bee34e13d
Christopher Noce <c@abdefg.com>
2019-01-18 14:06:07 -0500

    Update calendar.js

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#foobar').fullCalendar({
-    googleCalendarApiKey: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs', //'AIzaSyBuz_RmLdI8S4TV-1JJXVpiyRuLqRUinZk',
+    googleCalendarApiKey: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs',
     eventSources: [
       {
         googleCalendarId: 'concordps.org_classroom6880af80@group.calendar.google.com',
@@ -25,7 +25,3 @@ $(document).ready(function() {
     ]
   });
 });
-
-https://apis.google.com/_/scs/apps-static/_/js/k=oz.gapi.en.2lYDMsSK54U.O/m=client/rt=j/sv=1/d=1/ed=1/am=wQ/rs=AGLTcCMymAizLmRiAlJ4_BDLaoYc2P1-ag/cb=gapi.loaded_0
-
-function _onload() {window._init({"features":[1,1,1,1,0,1,1,1,0,1,0,1,0,0,0,1,0,0,0,1,1,1,0,1,0,0,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,1,1,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,0,0,1,0,0,0,0,1,1,0,0,1,0,0,1,0,0,0,1,1,1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,0,1,1,1,1,0,1,0,1,0,0,1,0,0,1,1,1,0,1,0,0,1,1,0,1,0,0,1,1,0,1,1,0,0,1,1,1,1,0,1,1,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,1,1,1,1,0,0,0],"loggedin":true,"cids":{"concordps.org_classroom6880af80@group.calendar.google.com":{"color":"#0f4b38","access":70,"title":"Concord Bands: 6th Grade Band"},"concordps.org_classroom60624685@group.calendar.google.com":{"color":"#5f6b02","access":70,"title":"Concord Bands: 7th \u0026 8th Grade Band"},"concordps.org_nbu2m6begv8r25refgt02umj1c@group.calendar.google.com":{"color":"#875509","access":70,"title":"Concord Bands: Elementary"},"concordps.org_classroomfa6b71ca@group.calendar.google.com":{"color":"#b1365f","access":70,"title":"Concord Bands: Jazz Band"},"concordps.org_classroom6d3a6565@group.calendar.google.com":{"color":"#691426","access":70,"title":"Concord Bands: Wind Ensemble"}},"view":"month","weekstart":0,"format24hour":false,"dateFieldOrder":0,"preloadStart":"20181229","preloadEnd":"20190207","container":"container","baseUrl":"/","imagePath":"//calendar.google.com/googlecalendar/images/","hostedDomain":{"name":"concordps.org","title":"Concord Public Schools","maplink":null},"timezone":"America/New_York","timezoneLocalized":"Eastern Time - New York","timezoneOffsetMs":-18000000,"nowMs":"1547837277832","timezoneNextTransitionMs":"1552201200000","timezoneNextOffsetMs":-14400000,"showNavigation":true,"showDateMarker":true,"showTabs":true,"showCalendarMenu":true,"showCurrentTime":true,"showTz":false,"showPrintButton":true,"showElementsLogo":false,"collapseAllday":false,"showSubscribeButton":true,"embedStyle":"WyJhdDplbWI6c3QiXQo\u003d","proxyUrl":"https://clients6.google.com","calendarApiVersion":"v3","developerKey":"AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"});}
