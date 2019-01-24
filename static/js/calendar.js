83cada2b86a2deaae9eeff1e84d73f7daeb036e4
Christopher Noce <c@abdefg.com>
2019-01-23 22:07:31 -0500

    many updates

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,15 +1,47 @@
+ics_sources = [
+    {url:'calendars/6.ics',event_properties:{color:'red'}},
+    {url:'calendars/78.ics',event_properties:{color:'orange'}},
+    {url:'calendars/elem.ics',event_properties:{color:'yellow'}},
+    {url:'calendars/jazz.ics',event_properties:{color:'green'}},
+    {url:'calendars/windens.ics',event_properties:{color:'blue'}},
+]
+
+
+function data_req (url, callback) {
+    req = new XMLHttpRequest()
+    req.addEventListener('load', callback)
+    req.open('GET', url)
+    req.send()
+}
+
+function add_recur_events() {
+    if (sources_to_load_cnt < 1) {
+        $('#calendar').fullCalendar('addEventSource', expand_recur_events)
+    } else {
+        setTimeout(add_recur_events, 30)
+    }
+}
+
+function load_ics(ics){
+    data_req(ics.url, function(){
+        $('#calendar').fullCalendar('addEventSource', fc_events(this.response, ics.event_properties))
+        sources_to_load_cnt -= 1
+    })
+}
+
 $(document).ready(function() {
-  $('#foobar').fullCalendar({
-    eventSources: [
-      // your event source
-        {
-          url: 'https://ical-to-json.herokuapp.com/convert.json?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fconcordps.org_classroom6880af80%2540group.calendar.google.com%2Fpublic%2Fbasic.ics', // use the `url` property
-          color: 'yellow',    // an option!
-          textColor: 'black'  // an option!
-        }
-
-        // any other sources...
-
-      ]
-    });
-  });
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultView: 'month',
+        defaultDate: '2016-03-01'
+    })
+    sources_to_load_cnt = ics_sources.length
+    for (ics of ics_sources) {
+        load_ics(ics)
+    }
+    add_recur_events()
+})
